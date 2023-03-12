b22ac9f142e6cb4ca1e287ca31ac629b3d5e7864
unknown <elasritaha191@gmail.com>
2023-03-12 20:50:25 +0100

    firstcommit

---
+++ FirstTest/app/src/main/java/com/example/firsttest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.firsttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        List<String> spinnerArray =  new ArrayList<String>();
+        spinnerArray.add("$");
+        spinnerArray.add("euro");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.spinner);
+        sItems.setAdapter(adapter);
+    }
+
+}
